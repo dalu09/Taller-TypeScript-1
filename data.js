01bdd27be8a88f5d2e59c4b0f59c78e572fdc0fa
Lucia Castillo <111028118+dalu09@users.noreply.github.com>
2024-05-05 21:51:34 -0500

    Final

---
+++ data.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.series = void 0;
+var series_1 = require("./series");
+exports.series = [
+    new series_1.Serie(1, "Breaking Bad", "AMC", 5),
+    new series_1.Serie(2, "Orange Is the New Black", "Netflix", 6),
+    new series_1.Serie(3, "Game of Thrones", "HBO", 7),
+    new series_1.Serie(4, "The Big Bang Theory", "CBS", 12),
+    new series_1.Serie(5, "Sherlock", "BBC", 4),
+    new series_1.Serie(6, "A Very English Scandal", "BBC", 2),
+];
